fdd64f822c9bdabc1efdececbc755827bb7ef6b2
Tayyab <tayyabkhalid993@gmail.com>
2025-06-14 22:38:31 +0500

    design landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import TextPressure from "@/components/scatter-text";
 import Image from "next/image";
+import SplitText from "@/components/split-text";
 import Link from "next/link";
 import Silk from '@/components/silk-background';
 
@@ -10,22 +11,22 @@ export default function Home() {
     <div className="">
 
       {/* background-animation */}
-<div className="absolute inset-0 z-[-1]">
-<Silk
-  speed={5}
-  scale={1}
-  color="#0575E6"
-  noiseIntensity={1.5}
-  rotation={0}
-/>
-  </div>
+      <div className="absolute inset-0 z-[-1]">
+        <Silk
+          speed={5}
+          scale={1}
+          color="#0575E6"
+          noiseIntensity={1.5}
+          rotation={0}
+        />
+      </div>
 
 
 
-  {/* Header */}
-      
+      {/* Header */}
+
       <header>
-        <nav className="flex justify-between items-center w-full px-5 ">
+        <nav className="flex justify-between items-center w-full px-5 font-bold">
           <Link href={"/"}><Image src={"/PocketTracerWhite.png"} width={200} height={200} alt="Pocket Tracer Logo"></Image></Link>
           <div>
             <ul className="flex justify-center items-center gap-3">
@@ -37,15 +38,43 @@ export default function Home() {
             </ul>
           </div>
           <div className="flex justify-center gap-3">
-        <button><Link className="underline-text" href={"/auth/login"}>LogIn</Link></button>
-        <button><Link className="underline-text" href={"/auth/sign-up"}>SignUp</Link></button>
-      </div>
+            <button><Link className="underline-text" href={"/auth/login"}>LogIn</Link></button>
+            <button><Link className="underline-text" href={"/auth/sign-up"}>SignUp</Link></button>
+          </div>
         </nav>
       </header>
 
 
 
-    {/* Landing Page */}
+      {/* Landing Page */}
+
+      <section className="flex flex-col justify-center items-center h-screen text-center text-white">
+
+
+        <SplitText
+          text="Stop Guessing. Start Tracking."
+          className="text-7xl font-semibold text-center pb-4"
+          delay={100}
+          duration={0.3}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+        />
+
+        <h1 className="text-7xl"></h1>
+        <p className="w-2xl mt-10 text-xl">Stop guessing and start tracking with confidence. Pocket Tracer transforms your financial data into clear visuals and professional reports, so you always know where your money stands.</p>
+        <p className="font-bold bg-[#8480fd] text-[#fff] rounded px-2">No accounting degree required.</p>
+      </section>
+
+
+
+
+
+
 
 
 
